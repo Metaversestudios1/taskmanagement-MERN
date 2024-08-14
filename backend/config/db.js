b831de91618e0560b5ecb242fcf183f1d86bd5be
nikita <nikita@gmail.com>
2024-08-14 18:02:19 +0530

    mongodb atlas connectd

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         // await mongoose.connect("mongodb://localhost:27017/taskmanagement");
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://socialtridentverse:ODNTcEyh1SJNJeW3@taskmanagement.xs0sq.mongodb.net/taskmanagement?retryWrites=true&w=majority");
         console.log('MongoDB connected');
     } catch (error) {
         if (error.message.includes('database exists')) {
