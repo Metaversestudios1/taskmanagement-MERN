1ccc7874f01184f2796038a5dd6ffc48d0ab4bea
nikita <nikita@gmail.com>
2024-08-16 10:09:32 +0530

    added config changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect("mongodb://localhost:27017/taskmanagement");
-        await mongoose.connect("mongodb+srv://socialtridentverse:ODNTcEyh1SJNJeW3@taskmanagement.xs0sq.mongodb.net/taskmanagement?retryWrites=true&w=majority");
+         await mongoose.connect(process.env.MONGO_URI);
+        //await mongoose.connect("mongodb+srv://socialtridentverse:ODNTcEyh1SJNJeW3@taskmanagement.xs0sq.mongodb.net/taskmanagement?retryWrites=true&w=majority");
         console.log('MongoDB connected');
     } catch (error) {
         if (error.message.includes('database exists')) {
