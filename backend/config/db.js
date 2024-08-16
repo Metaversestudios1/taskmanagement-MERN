a6f472275e792aad31849a18b487ad0005c836be
nikita <nikita@gmail.com>
2024-08-16 11:46:21 +0530

    config file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-         await mongoose.connect(process.env.MONGO_URI);
+    try {""
+         await mongoose.connect("mongodb+srv://socialtridentverse:ODNTcEyh1SJNJeW3@taskmanagement.xs0sq.mongodb.net/?retryWrites=true&w=majority&appName=taskmanagement");
+         
         //await mongoose.connect("mongodb+srv://socialtridentverse:ODNTcEyh1SJNJeW3@taskmanagement.xs0sq.mongodb.net/taskmanagement?retryWrites=true&w=majority");
         console.log('MongoDB connected');
     } catch (error) {
