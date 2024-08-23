594506cb1786d8a630dd5943b73a1e1eebea5cf2
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-08-23 16:49:17 +0530

    task

--- backend/routes/TaskRoutes.js
+++ backend/routes/TaskRoutes.js
@@ -4,43 +4,28 @@ const { insertTask, getAllTask, deleteTask, updateTask, getSingletask,downloadFi
 
 const router = express.Router();
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'uploads/');
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, `${Date.now()}-${file.originalname}`);
-//     }
-//   });
-  
-  
-// const upload = multer({ 
-//     storage: storage,
-//     limits: { fileSize: 10 * 1024 * 1024 }, // Limit to 10MB
-//     fileFilter: (req, file, cb) => { 
-//       // Allow all file types
-//       if (file.mimetype) {
-//         cb(null, true);
-//       } else {
-//         cb(new Error('Invalid file type, only certain files are allowed!'), false);
-//       }
-//     }
-//   });
-
-const storage = multer.memoryStorage();
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Limit to 10MB
-  fileFilter: (req, file, cb) => { 
-    if (file.mimetype) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type, only certain files are allowed!'), false);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`);
     }
-  }
-});
-
+  });
+  
+  
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 }, // Limit to 10MB
+    fileFilter: (req, file, cb) => { 
+      // Allow all file types
+      if (file.mimetype) {
+        cb(null, true);
+      } else {
+        cb(new Error('Invalid file type, only certain files are allowed!'), false);
+      }
+    }
+  });
   
 router.get('/gettask', getAllTask);
 router.post('/inserttask', upload.single('attachment'), insertTask);
