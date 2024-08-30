3dbd818cea9783a9a5eb184daa098448b351df37
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-08-30 16:18:27 +0530

    Update Employees.js

--- frontend/src/components/employees/Employees.js
+++ frontend/src/components/employees/Employees.js
@@ -138,7 +138,7 @@ const Employees = () => {
         </div>
       </div>
 
-      {loader && <div className="flex justify-center"><div
+      {loader && <div className="absolute h-full w-full top-64  flex justify-center items-center"><div
         class=" flex justify-center h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
         role="status">
         <span
